5d7997a36a3f7f6af5e3e387b26dc45119dd5f03
danieleppler <danieleppler51@gmail.com>
2020-05-04 20:36:28 +0300

    started complex,dividing the equation into 2 parts,left and right

--- solver.cpp
+++ solver.cpp
@@ -8,171 +8,228 @@
 
 using namespace std;
 using namespace solver;
+
+//////////////////////////////////////
+//////////// RealVariable ////////////
+//////////////////////////////////////
+
+double solver::operator==(RealVariable rv, double d){
+    return d;
+}
+
+double solver::operator==(double d, RealVariable rv){
+    return solver::operator==(rv,d);
+}
+
+double solver::operator==(RealVariable rv, RealVariable rv1){
+    return 0.0;
+}
+
+RealVariable& solver::operator*(RealVariable rv, double d){
+    rv._pow2._coff *= d;
+    rv._pow1._coff *= d;
+    rv._coff *= d;
+    return rv;
+}
+RealVariable& solver::operator*(double d, RealVariable rv){
+    return solver::operator*(rv, d);
+}
+
+RealVariable& solver::operator/(RealVariable rv, double d){
+    rv._pow2._coff /= d;
+    rv._pow1._coff /= d;
+    rv._coff /= d;
+    return rv;
+}
+RealVariable& solver::operator/(RealVariable rv, int i){
+    rv._pow2._coff /= i;
+    rv._pow1._coff /= i;
+    rv._coff /= i;
+    return rv;
+}
+
+RealVariable& solver::operator+(RealVariable rv, RealVariable rv1){
+    rv._pow2._coff += rv1._pow2._coff;
+    rv._pow1._coff += rv1._pow1._coff;
+    rv._coff += rv1._coff;
+    return rv;
+}
+RealVariable& solver::operator+(RealVariable rv, double d){
+    rv._coff += d;
+    return rv;
+}
+RealVariable& solver::operator+(double d, RealVariable rv){
+    rv._coff += d;
+    return rv;
+}
+
+RealVariable& solver::operator-(RealVariable rv, RealVariable rv1){
+    rv._pow2._coff -= rv1._pow2._coff;
+    rv._pow1._coff -= rv1._pow1._coff;
+    rv._coff -= rv1._coff;
+    return rv;
+}
+RealVariable& solver::operator-(RealVariable rv, double d){
+    rv._coff -= d;
+    return rv;
+}
+RealVariable& solver::operator-(RealVariable rv, int i){
+    rv._coff -= i;
+    return rv;
+}
+
+// maybe no?
+RealVariable solver::operator^(RealVariable rv, int i){
+    return rv;
+}
+
+
+///////////////////////////////////////
+/////////// ComplexVariable ///////////
+///////////////////////////////////////
+
+
+ComplexVariable solver::operator==(ComplexVariable cv, double d){
+    return cv;
+}
+
+ComplexVariable solver::operator==(double d, ComplexVariable cv){
+    return cv==d;
+}
+
+ComplexVariable solver::operator==(ComplexVariable cv1, complex<double> cv){
+    return cv1;
+}
+
+ComplexVariable solver::operator==(ComplexVariable cv1, ComplexVariable cv){
+    return cv1;
+}
+
+
+ComplexVariable& solver::operator*(double d, ComplexVariable& cv){
     
-    //////////////////////////////////////
-    //////////// RealVariable ////////////
-    //////////////////////////////////////
+    return cv;
+}
 
-    double solver::operator==(RealVariable rv, double d){
-        return d;
-    }
-    double solver::operator==(double d, RealVariable rv){
-        return solver::operator==(rv,d);
-    }
-    double solver::operator==(RealVariable rv, RealVariable rv1){
-        return 0.0;
-    }
+ComplexVariable& solver::operator*(ComplexVariable& cv, double d){
+    return d*cv;
+}
 
-    RealVariable& solver::operator*(RealVariable rv, double d){
-        rv._pow2._coff *= d;
-        rv._pow1._coff *= d;
-        rv._coff *= d;
-        return rv;
-    }
-    RealVariable& solver::operator*(double d, RealVariable rv){
-        return solver::operator*(rv, d);
-    }
+//why operator * for double AND for int?
 
-    RealVariable& solver::operator/(RealVariable rv, double d){
-        rv._pow2._coff /= d;
-        rv._pow1._coff /= d;
-        rv._coff /= d;
-        return rv;
-    }
-    RealVariable& solver::operator/(RealVariable rv, int i){
-        rv._pow2._coff /= i;
-        rv._pow1._coff /= i;
-        rv._coff /= i;
-        return rv;
-    }
+ComplexVariable& solver::operator*(ComplexVariable& cv, int i){
+    return cv;
+}
 
-    RealVariable& solver::operator+(RealVariable rv, RealVariable rv1){
-        rv._pow2._coff += rv1._pow2._coff;
-        rv._pow1._coff += rv1._pow1._coff;
-        rv._coff += rv1._coff;
-        return rv;
-    }
-    RealVariable& solver::operator+(RealVariable rv, double d){
-        rv._coff += d;
-        return rv;
-    }
-    RealVariable& solver::operator+(double d, RealVariable rv){
-        rv._coff += d;
-        return rv;
-    }
+ComplexVariable& solver::operator*(int i, ComplexVariable& cv){
+    return cv*i;
+}
 
-    RealVariable& solver::operator-(RealVariable rv, RealVariable rv1){
-        rv._pow2._coff -= rv1._pow2._coff;
-        rv._pow1._coff -= rv1._pow1._coff;
-        rv._coff -= rv1._coff;
-        return rv;
-    }
-    RealVariable& solver::operator-(RealVariable rv, double d){
-        rv._coff -= d;
-        return rv;
-    }
-    RealVariable& solver::operator-(RealVariable rv, int i){
-        rv._coff -= i;
-        return rv;
-    }
+ComplexVariable& solver::operator+(double d, ComplexVariable& cv){
 
-    // maybe no?
-    RealVariable solver::operator^(RealVariable rv, int i){
-        return rv;
-    }
+}
+ComplexVariable& solver::operator+(ComplexVariable& cv, double d){
+    return d*cv;
+}
+
+//why operator + for double AND int?
+
+ComplexVariable& solver::operator+(ComplexVariable& cv, int i){
+    return cv;
+}
+//std::complex + std::complex is defined
+ComplexVariable& solver::operator+(ComplexVariable cv1, ComplexVariable& cv){
+    return cv;
+}
+
+//adding just the variable without no power.std::complex * int is defined
+ComplexVariable& solver::operator+(ComplexVariable& cv1, complex<double> cv){
+    return cv1;
+}
+
+ComplexVariable& solver::operator-(double d, ComplexVariable& cv){
+    return cv;
+}
+
+//std::complex - std::complex is defined
 
 
-    ///////////////////////////////////////
-    /////////// ComplexVariable ///////////
-    ///////////////////////////////////////
+   /*
+    * if cv1 is from the form of 'y',it will signs that is completely the same complex
+    * numbers,and then add just one to the cof of pow1
+    */
+
+    ComplexVariable& solver::operator-(ComplexVariable cv1, ComplexVariable& cv){
+       return cv1;
+
+   }
 
 
-    ComplexVariable solver::operator==(ComplexVariable cv, double d){
-        return cv;
-    }
+//why - for double AND int?
+ComplexVariable& solver::operator-(ComplexVariable& cv, int i){
+    return cv;
+}
 
-    ComplexVariable solver::operator==(double d, ComplexVariable cv){
-        return cv;
-    }
+ComplexVariable& solver::operator-(ComplexVariable& cv, double d){
+    return d-cv;
+}
 
-    ComplexVariable solver::operator==(ComplexVariable cv1, complex<double> cv){
-        return cv1;
-    }
-    ComplexVariable solver::operator==(ComplexVariable cv1, ComplexVariable cv){
-        return cv1;
-    }
+ComplexVariable& solver::operator-(ComplexVariable& cv1, complex<double> cv){
+    if(cv1.flipSigns)
+        cv*=(-1);
+    cv1.v-=cv;
+    return cv1;
+}
 
-    ComplexVariable& solver::operator*(double d, ComplexVariable cv){
-        return cv;
-    }
-    ComplexVariable& solver::operator*(ComplexVariable cv, double d){
-        return cv;
-    }
-    ComplexVariable& solver::operator*(ComplexVariable cv, int i){
-        return cv;
-    }
-    ComplexVariable& solver::operator*(int i, ComplexVariable cv){
-        return cv;
-    }
-    
-    ComplexVariable& solver::operator+(double d, ComplexVariable cv){
-        return cv;
-    }
-    ComplexVariable& solver::operator+(ComplexVariable cv, double d){
-        return cv;
-    }
+ComplexVariable& solver::operator/(double d, ComplexVariable& cv){
+    if(cv.flipSigns)
+        d*=(-1);
+    cv._pow1.v.real(d/cv._pow1.v.real());
+    cv._pow2.v.real(d/cv._pow2.v.real());
+    cv.v.real(d/cv.v.real());
+    return cv;
+}
 
-    ComplexVariable& solver::operator+(ComplexVariable cv, int i){
-        return cv;
-    }
-    ComplexVariable& solver::operator+(ComplexVariable cv1, ComplexVariable cv){
-        return cv;
-    }
-    ComplexVariable& solver::operator+(ComplexVariable cv1, complex<double> cv){
-        return cv1;
-    }
-    ComplexVariable& solver::operator-(double d, ComplexVariable cv){
-        return cv;
-    }
-    ComplexVariable& solver::operator-(ComplexVariable cv1, ComplexVariable cv){
-        return cv;
-    }
+ComplexVariable& solver::operator/(ComplexVariable& cv, int i){
+    if(cv.flipSigns)
+        i*=(-1);
+    cv._pow1.v.real(cv._pow1.v.real()/i);
+    cv._pow2.v.real(cv._pow2.v.real()/i);
+    cv.v.real(cv.v.real()/i);
+    return cv;
+}
 
-    ComplexVariable& solver::operator-(ComplexVariable cv, int i){
-        return cv;
+ComplexVariable solver::operator^(ComplexVariable& cv, int i){
+    if(cv.flipSigns)
+        i*=-1;
+    if(cv.Init)
+    {
+        cv._pow2.v.real(1);
+    } else {
+        cv._pow1.v.real(pow(cv._pow1.v.real(), 2));
+        cv._pow1.v.imag(pow(cv._pow1.v.imag(), 2));
+        cv._pow2.v.real(pow(cv._pow2.v.real(), 2));
+        cv._pow2.v.imag(pow(cv._pow2.v.imag(), 2));
+        cv.v.real(pow(cv.v.real(), 2));
+        cv.v.imag(pow(cv.v.imag(), 2));
     }
-    ComplexVariable& solver::operator-(ComplexVariable cv, double d){
-        return cv;
-    }
-    ComplexVariable& solver::operator-(ComplexVariable cv1, complex<double> cv){
-        return cv1;
-    }
+    return cv;
+}
 
-    ComplexVariable& solver::operator/(double d, ComplexVariable cv){
-        return cv;
-    }
-    ComplexVariable& solver::operator/(ComplexVariable cv, int i){
-        return cv;
-    }
-
-    ComplexVariable solver::operator^(ComplexVariable cv, int i){
-        return cv;
-    }
-
-    double solver::solve(double d){
-        return d;
-    }
-    complex<double> solver::solve(ComplexVariable y){
-        complex<double> d;
-        return d;
-    }
+double solver::solve(RealVariable x){
+    return 0;
+}
+complex<double> solver::solve(ComplexVariable y){
+    complex<double> d;
+    return d;
+}
 
 
 // /**
 //  * @param RealVariable rv
-//  * 
+//  *
 //  * result of < a*x^2 + b*x + c = 0 >
-//  * 
+//  *
 //  * @result vector with x1 and x2
 //  */
 // vector<double> Quadratic_Equation(RealVariable rv){
@@ -182,8 +239,8 @@ using namespace solver;
 //     if (delta > 0){
 //         double x1 = (-1*b + sqrt(delta)) / (2*a);
 //         double x2 = (-1*b - sqrt(delta)) / (2*a);
-//         ans.push_back(x1); 
-//         ans.push_back(x2); 
+//         ans.push_back(x1);
+//         ans.push_back(x2);
 //     }
 //     else if (delta == 0){
 //         double x1 = (-1*b) / (2*a);
--- solver.hpp
+++ solver.hpp
@@ -3,28 +3,28 @@
 #include <complex.h>
 using namespace std;
 namespace solver{
-    
+
     class RealVariable{
     private:
         struct pow2{
             double _coff;
         } _pow2;
-        struct pow1{ 
+        struct pow1{
             double _coff;
         } _pow1;
         double _coff, _res;
-    
+
     public:
-		RealVariable(double d_pow2, double d_pow1, double d){
-			_pow2._coff = d_pow2;
-			_pow1._coff = d_pow1;
-			_coff = d;
-		}
-		RealVariable(){
-			_pow2._coff = 0;
-			_pow1._coff = 0;
-			_coff = 0;
-		}
+        RealVariable(double d_pow2, double d_pow1, double d){
+            _pow2._coff = d_pow2;
+            _pow1._coff = d_pow1;
+            _coff = d;
+        }
+        RealVariable(){
+            _pow2._coff = 0;
+            _pow1._coff = 0;
+            _coff = 0;
+        }
 
 
 // 2*x+2 == x^2*3-5 / 9
@@ -54,64 +54,85 @@ namespace solver{
 
     class ComplexVariable{
     private:
-        struct pow2{
-            double _re, _im;
-        } _pow2;
-        struct pow1{ 
-            double _re, _im;
-        } _pow1;
-        double _re, _im, _res;
-    
+        /*
+         * changed the class members to be std::complex so it will be more efficient
+         * in the init of the variable,when we want to multiply/divide we will conflict a problem because we are dealing with zero
+         * and we will lose the value.i generated a boolean
+         *  added boolean variable to check whatever we passed the == operator and we need to flip the sign of all the number
+         *  afterwords
+         *  TRYING SPLITTING THE EQUATION INTO 2 HALVES
+         */
+        struct right {
+            struct pow2 {
+                std::complex<double> v;
+            } _pow2;
+            struct pow1 {
+                std::complex<double> v;
+            } _pow1;
+            std::complex<double> v;
+            bool Init = true;
+        } right;
+        struct left{
+            struct pow2 {
+                std::complex<double> v;
+            } _pow2;
+            struct pow1 {
+                std::complex<double> v;
+            } _pow1;
+            std::complex<double> v;
+            bool Init = true;
+        } left;
+
+
     public:
-        ComplexVariable(double re_pow2, double im_pow2, double re_pow1, double im_pow1, double re, double im ){
-            _pow2._re = re_pow2;
-            _pow2._im = im_pow2;
-            _pow1._re = re_pow1;
-            _pow1._im = im_pow1;
-			_re = re;
-			_im = im;
-        }
-        ComplexVariable(){
-            _pow2._re = 0;
-            _pow2._im = 0;
-            _pow1._re = 0;
-            _pow1._im = 0;
-			_re = 0;
-			_im = 0;
+        /*
+        ComplexVariable(std::complex<double> pow2,std::complex<double> pow1, std::complex<double> pow0 ){
+            _pow2.v=pow2;
+            _pow1.v=pow1;
+            v=pow0;
         }
 
+        ComplexVariable(){
+            _pow2.v = (0,0);
+            _pow1.v = (0,0);
+            v=(0,0);
+            demo=(1,0);
+        }
+        */
+        /*
+         * changed all the friend functions to get refrence variable of cv instead of cv so the changed wil be saved
+         */
         friend ComplexVariable operator==(ComplexVariable cv, double d);
         friend ComplexVariable operator==(double d, ComplexVariable cv);
         friend ComplexVariable operator==(ComplexVariable cv1, complex<double> cv);
         friend ComplexVariable operator==(ComplexVariable cv1, ComplexVariable cv);
 
+        friend ComplexVariable& operator*(double d, ComplexVariable& cv);
+        friend ComplexVariable& operator*(ComplexVariable& cv, double d);
+        friend ComplexVariable& operator*(ComplexVariable& cv, int i);
+        friend ComplexVariable& operator*(int i, ComplexVariable& cv);
 
-        friend ComplexVariable& operator*(double d, ComplexVariable cv);
-        friend ComplexVariable& operator*(ComplexVariable cv, double d);
-        friend ComplexVariable& operator*(ComplexVariable cv, int i);
-        friend ComplexVariable& operator*(int i, ComplexVariable cv);
+        friend ComplexVariable operator^(ComplexVariable& cv, int i);
 
-        friend ComplexVariable operator^(ComplexVariable cv, int i);
+        friend ComplexVariable& operator+(double d, ComplexVariable& cv);
+        friend ComplexVariable& operator+(ComplexVariable& cv, double d);
+        friend ComplexVariable& operator+(ComplexVariable& cv, int i);
+        friend ComplexVariable& operator+(ComplexVariable cv1, ComplexVariable& cv);
+        friend ComplexVariable& operator+(ComplexVariable& cv1, complex<double> cv);
 
-        friend ComplexVariable& operator+(double d, ComplexVariable cv);
-        friend ComplexVariable& operator+(ComplexVariable cv, double d);
-        friend ComplexVariable& operator+(ComplexVariable cv, int i);
-        friend ComplexVariable& operator+(ComplexVariable cv1, ComplexVariable cv);
-        friend ComplexVariable& operator+(ComplexVariable cv1, complex<double> cv);
-
-        friend ComplexVariable& operator-(double d, ComplexVariable cv);
-        friend ComplexVariable& operator-(ComplexVariable cv1, ComplexVariable cv);
-        friend ComplexVariable& operator-(ComplexVariable cv, int i);
-        friend ComplexVariable& operator-(ComplexVariable cv, double d);
-        friend ComplexVariable& operator-(ComplexVariable cv1, complex<double> cv);
+        friend ComplexVariable& operator-(double d, ComplexVariable& cv);
+        friend ComplexVariable& operator-(ComplexVariable cv1, ComplexVariable& cv);
+        friend ComplexVariable& operator-(ComplexVariable& cv, int i);
+        friend ComplexVariable& operator-(ComplexVariable& cv, double d);
+        friend ComplexVariable& operator-(ComplexVariable& cv1, complex<double> cv);
 
 
-        friend ComplexVariable& operator/(double d, ComplexVariable cv);
-        friend ComplexVariable& operator/(ComplexVariable cv, int i);
+        friend ComplexVariable& operator/(double d, ComplexVariable& cv);
+        friend ComplexVariable& operator/(ComplexVariable& cv, int i);
 
     };
 
-    double solve(double d);
+    double solve(RealVariable x);
     complex<double> solve(ComplexVariable y);
 
 }
